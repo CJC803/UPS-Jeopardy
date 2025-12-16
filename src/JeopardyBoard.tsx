68f38a951145baa69f5958c317815d8b2812c631
CJC803 <cjoshcummin@hotmail.com>
2025-12-16 12:36:58 -0500

    Update JeopardyBoard.tsx

--- src/JeopardyBoard.tsx
+++ src/JeopardyBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from "react";
 
 // Stubbed framer-motion for Preview with a CSS-based animation
 const MotionDiv = (props: any) => {
